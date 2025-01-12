9136a761098b0c3a4d75c7667016b169228af896
OshaniKR <kawshalyaoshani5@gmail.com>
2025-01-12 12:47:08 +0530

    Profile_completed

--- social/lib/pages/profile_page.dart
+++ social/lib/pages/profile_page.dart
@@ -1,11 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ProfilePage extends StatefulWidget {
-  const ProfilePage({Key? key});
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
   State<ProfilePage> createState() => _ProfilePageState();
@@ -16,45 +16,6 @@ class _ProfilePageState extends State<ProfilePage> {
   final userCollection = FirebaseFirestore.instance.collection("Users");
   final storageRef = FirebaseStorage.instance.ref();
 
-  Future<void> editField(String field, String currentValue) async {
-    String newValue = currentValue;
-    TextEditingController textController =
-        TextEditingController(text: currentValue);
-
-    await showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        backgroundColor: Colors.orange.shade800,
-        title: Text("Edit $field", style: TextStyle(color: Colors.white)),
-        content: TextField(
-          controller: textController,
-          autofocus: true,
-          style: TextStyle(color: Colors.white),
-          onChanged: (value) => newValue = value,
-          decoration: InputDecoration(
-            hintText: 'Enter new $field',
-            hintStyle: TextStyle(color: Colors.white),
-          ),
-        ),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context),
-            child: Text('Cancel', style: TextStyle(color: Colors.white)),
-          ),
-          TextButton(
-            onPressed: () {
-              if (newValue.trim().isNotEmpty) {
-                userCollection.doc(currentUser.email).update({field: newValue});
-                Navigator.of(context).pop();
-              }
-            },
-            child: Text('Save', style: TextStyle(color: Colors.white)),
-          ),
-        ],
-      ),
-    );
-  }
-
   Future<void> uploadImage(String imageType) async {
     try {
       final pickedFile =
@@ -63,44 +24,24 @@ class _ProfilePageState extends State<ProfilePage> {
       if (pickedFile != null) {
         final imagePath =
             'user_profiles/${currentUser.email}/$imageType/${pickedFile.name}';
-
-        // Read the file as bytes for web compatibility
         final bytes = await pickedFile.readAsBytes();
-
-        // Upload the file to Firebase Storage
         final uploadTask = storageRef.child(imagePath).putData(bytes);
 
-        // Wait for upload to complete
         await uploadTask;
-
-        // Get the download URL of the uploaded image
         String imageUrl = await storageRef.child(imagePath).getDownloadURL();
-
-        // Update the Firestore document with the image URL
         await userCollection
             .doc(currentUser.email)
             .update({imageType: imageUrl});
 
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('${imageType} uploaded successfully.')),
+          SnackBar(content: Text('$imageType uploaded successfully.')),
         );
       }
     } catch (e) {
-      print("Error uploading $imageType: $e");
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
             content: Text('Error uploading $imageType. Please try again.')),
       );
-    }
-  }
-
-  Future<String> getImageUrl(String path) async {
-    try {
-      String downloadUrl = await storageRef.child(path).getDownloadURL();
-      return downloadUrl;
-    } catch (e) {
-      print("Error getting image URL: $e");
-      return '';
     }
   }
 
@@ -127,96 +68,75 @@ class _ProfilePageState extends State<ProfilePage> {
             if (userData == null) {
               return Center(child: Text('No data found.'));
             }
+
             return ListView(
-              padding: EdgeInsets.symmetric(vertical: 20),
+              padding: EdgeInsets.all(20),
               children: [
-                CircleAvatar(
-                  radius: 50,
-                  backgroundImage: userData['profileImage'] != null
-                      ? NetworkImage(userData['profileImage'])
-                      : null,
-                  child: userData['profileImage'] == null
-                      ? Icon(Icons.person,
-                          size: 50, color: Colors.orange.shade800)
-                      : null,
-                ),
-                SizedBox(height: 10),
-                Center(
-                  child: Column(
-                    children: [
-                      Text(userData['name'] ?? 'No Name',
-                          style: TextStyle(
-                              fontSize: 20, color: Colors.orange.shade800)),
-                      Text(userData['email'] ?? 'No Email',
-                          style: TextStyle(
-                              fontSize: 16, color: Colors.orange.shade800)),
-                    ],
-                  ),
+                Stack(
+                  alignment: Alignment.bottomRight,
+                  children: [
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundImage: userData['profileImage'] != null
+                          ? NetworkImage(userData['profileImage'])
+                          : null,
+                      child: userData['profileImage'] == null
+                          ? Icon(Icons.person,
+                              size: 50, color: Colors.orange.shade800)
+                          : null,
+                    ),
+                    IconButton(
+                      onPressed: () => uploadImage('profileImage'),
+                      icon: Icon(Icons.camera_alt, color: Colors.white),
+                      color: Colors.orange.shade800,
+                    ),
+                  ],
                 ),
                 SizedBox(height: 20),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 25),
-                  child: Text('My Details',
-                      style: TextStyle(color: Colors.orange.shade800)),
-                ),
                 MyTextBox(
                   text: userData['name'] ?? 'No Name',
                   sectionName: "Name",
-                  onPressed: () => editField('name', userData['name'] ?? ''),
                 ),
                 MyTextBox(
-                  text: userData['homeTown'] ?? 'No hometown',
+                  text: userData['email'] ?? 'No Email',
+                  sectionName: "Email",
+                ),
+                MyTextBox(
+                  text: userData['homeTown'] ?? 'No Hometown',
                   sectionName: "Home Town",
-                  onPressed: () =>
-                      editField('homeTown', userData['homeTown'] ?? ''),
                 ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 25),
-                  child: ElevatedButton(
-                    onPressed: () => uploadImage('profileImage'),
-                    child: Text("Upload Profile Image"),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 25),
-                  child: ElevatedButton(
-                    onPressed: () => uploadImage('petImage'),
-                    child: Text("Upload Pet's Image"),
-                  ),
+                MyTextBox(
+                  text: userData['petName'] ?? 'No Pet Name',
+                  sectionName: "Pet Name",
                 ),
                 SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () => uploadImage('petImage'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.orange,
+                  ),
+                  child: Text("Upload Pet's Image"),
+                ),
                 if (userData['petImage'] != null)
-                  Column(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 25),
-                        child: Text('My Pet\'s Image',
-                            style: TextStyle(color: Colors.orange.shade800)),
-                      ),
-                      SizedBox(height: 10),
-                      FutureBuilder<String>(
-                        future: getImageUrl(userData['petImage']),
-                        builder: (context, imageSnapshot) {
-                          if (imageSnapshot.connectionState ==
-                              ConnectionState.waiting) {
-                            return CircularProgressIndicator();
-                          } else if (imageSnapshot.hasError) {
-                            return Icon(Icons.error,
-                                color: Colors.orange.shade800);
-                          } else if (imageSnapshot.hasData) {
-                            return Image.network(
-                              imageSnapshot.data!,
-                              height: 150,
-                              width: 150,
-                              fit: BoxFit.cover,
-                            );
-                          } else {
-                            return Icon(Icons.error,
-                                color: Colors.orange.shade800);
-                          }
-                        },
-                      ),
-                    ],
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Pet's Image",
+                          style: TextStyle(
+                              fontSize: 16, color: Colors.orange.shade800),
+                        ),
+                        SizedBox(height: 10),
+                        Image.network(
+                          userData['petImage'],
+                          height: 150,
+                          width: 150,
+                          fit: BoxFit.cover,
+                        ),
+                      ],
+                    ),
                   ),
               ],
             );
@@ -231,37 +151,32 @@ class _ProfilePageState extends State<ProfilePage> {
 class MyTextBox extends StatelessWidget {
   final String text;
   final String sectionName;
-  final VoidCallback onPressed;
 
   const MyTextBox({
     required this.text,
     required this.sectionName,
-    required this.onPressed,
     Key? key,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onPressed,
-      child: Container(
-        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-        decoration: BoxDecoration(
-          color: Color.fromARGB(255, 246, 210, 155),
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: Row(
-          children: [
-            Expanded(
-              child:
-                  Text(text, style: TextStyle(color: Colors.orange.shade800)),
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        children: [
+          Text(
+            "$sectionName:",
+            style: TextStyle(
+                fontWeight: FontWeight.bold, color: Colors.orange.shade800),
+          ),
+          SizedBox(width: 10),
+          Expanded(
+            child: Text(
+              text,
+              style: TextStyle(color: Colors.orange.shade800),
             ),
-            IconButton(
-              icon: Icon(Icons.edit, color: Colors.orange.shade800),
-              onPressed: onPressed,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
